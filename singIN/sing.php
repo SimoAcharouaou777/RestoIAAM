9229675c583511256f08af9055fe5dd819618a52
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-05 11:33:02 +0100

    working in prepare statement

--- singIN/sing.php
+++ singIN/sing.php
@@ -30,9 +30,17 @@ if (isset($_POST['submit_login'])) {
 
   $username = $_POST['username_log'];
     $password_log = $_POST['password_log'];
-    $sql = "SELECT * FROM users WHERE name='$username'";
-    $result = mysqli_query($connect, $sql);
-    $row = mysqli_fetch_assoc($result);
+    $sql = "SELECT * FROM users WHERE name= ?";
+
+    $stmt = mysqli_prepare($connect, $sql);
+    if($stmt){
+      mysqli_stmt_bind_param ($stmt, "s", $username );
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      $row = mysqli_fetch_assoc($result);
+      mysqli_stmt_close($stmt);
+    }
+    // $row = mysqli_fetch_assoc($result);
     
     if (mysqli_num_rows($result) > 0) {
         if (password_verify($password_log  , $row['password'])) {
